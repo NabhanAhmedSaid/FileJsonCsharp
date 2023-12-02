7daf5d15ee00f4f94faf16640e1df85ff10c9cf7
NabhanAhmedSaid <100470531+NabhanAhmedSaid@users.noreply.github.com>
2023-12-02 15:24:27 +0400

    Add files via upload

---
+++ fileJson/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+using System.Text;
+
+
+
+class Program
+{
+    static void Main(){
+    
+
+        var now  = DateTime.Now.TimeOfDay;
+
+        var NameJson = "{\"Name\":\"Nabhan\"";
+        var Time = $"{NameJson},\"Time\":\"{now.Hours}\"";
+        var finalJson = $"{Time}"+"}";
+
+       
+
+        JsonDocument jsonDocument = JsonDocument.Parse(finalJson);
+
+          JsonElement root =  jsonDocument.RootElement;
+          
+           Console.WriteLine("Enter name of file");
+            var NameFile = Console.ReadLine();
+
+
+           //string path = @"/home/kali/Desktop/fileJson/myText.txt";
+          var lac = NameFile+".json";
+
+           File.WriteAllText(lac,finalJson);
+            Console.WriteLine($"{lac} is created");
+    }
+     
+       
+        
+   
+}
